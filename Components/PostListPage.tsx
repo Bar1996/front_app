9a1f74ada5166b8139ad1865a092dff38ed9cc64
Bar1996 <bar3668@gmail.com>
2024-05-20 21:26:02 +0300

    Add refresh screen

--- Components/PostListPage.tsx
+++ Components/PostListPage.tsx
@@ -1,52 +1,85 @@
-import { FC, useEffect, useState } from "react";
-import { FlatList, StyleSheet, Button, TouchableOpacity, ActivityIndicator, View } from "react-native";
+import { FC, useCallback, useEffect, useState } from "react";
+import { FlatList, StyleSheet, Button, TouchableOpacity, View, RefreshControl, ScrollView } from "react-native";
 import PostListRow from "./PostListRow";
 import PostModel, { Post } from "../Model/PostModel";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import ContentLoader, { Facebook } from 'react-content-loader/native';
 
 const PostListPage: FC<{ navigation: any }> = ({ navigation }) => {
     const [data, setData] = useState<Post[]>([]);
     const [isLoading, setIsLoading] = useState(false);
-  
+    const [refreshing, setRefreshing] = useState(false);
+    
 
     useEffect(() => {
         const unsubscribe = navigation.addListener('focus', async () => {
-            setIsLoading(true);
-            let posts: Post[] = [];
-            try {
-                posts = await PostModel.getAllPosts();
-            } catch (err) {
-                console.log("Failed fetching posts: " + err);
-                setData([]);
-            }finally {
-                setIsLoading(false);
-              }
-            setData(posts);
+            
+            PostPosts();
+            // setIsLoading(true);
+            // let posts: Post[] = [];
+            // try {
+            //     posts = await PostModel.getAllPosts();
+            // } catch (err) {
+            //     console.log("Failed fetching posts: " + err);
+            //     setData([]);
+            // } finally {
+            //     setTimeout(() => {
+            //         setIsLoading(false);
+            //     }, 1500);
+            // }
+            // setData(posts);
         });
-    
+
         return () => unsubscribe();
     }, [navigation]);
+
+    const PostPosts = async () => {
+        setIsLoading(true);
+        let posts: Post[] = [];
+        try {
+            posts = await PostModel.getAllPosts();
+        } catch (err) {
+            console.log("Failed fetching posts: " + err);
+            setData([]);
+        } finally {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 1500);
+        }
+        setData(posts);
+    }
 
     useEffect(() => {
         navigation.setOptions({
             headerRight: () => (
-                // <Button
-                //     onPress={() => navigation.navigate('AddNewPost')}
-                //     title="Add"
-                // />
                 <TouchableOpacity onPress={() => navigation.navigate('AddNewPost')} >
-                <Ionicons name="add-circle" size={30} style={styles.icon} />
-              </TouchableOpacity>
+                    <Ionicons name="add-circle" size={30} style={styles.icon} />
+                </TouchableOpacity>
             ),
         });
     }, [navigation]);
 
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        PostPosts();
+        setTimeout(() => {
+          setRefreshing(false);
+        }, 2000);
+      }, []);
+
     return (
         <View style={styles.container}>
             {isLoading ? (
-                <ActivityIndicator size="large" color="#007BFF" style={styles.loader} />
+                <View style={styles.loaderContainer}>
+                    <View style={styles.loader}><Facebook /></View>
+                    <View style={styles.loader}><Facebook /></View>
+                    <View style={styles.loader}><Facebook /></View>
+                </View>
             ) : (
                 <FlatList
+                refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
                     style={styles.flatList}
                     data={data}
                     keyExtractor={(item, index) => item.id || `temp-post-${index}`}
@@ -73,15 +106,17 @@ const styles = StyleSheet.create({
     flatList: {
         flex: 1,
     },
-    loader: {
+    loaderContainer: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+    },
+    loader: {
+        margin: 50,  // Adjust the margin as needed
     },
     icon: {
         marginRight: 8,
         color: "#007BFF",
-      },
+    },
 });
 
 export default PostListPage;
--- Components/UserPostList.tsx
+++ Components/UserPostList.tsx
@@ -1,43 +1,80 @@
-import { FC, useEffect, useState } from "react";
-import { FlatList, StyleSheet, Button, View, ActivityIndicator } from "react-native";
+import { FC, useCallback, useEffect, useState } from "react";
+import { FlatList, StyleSheet, Button, View, ActivityIndicator, RefreshControl } from "react-native";
 import PostListRow from "./PostListRow";
 import PostModel, { Post } from "../Model/PostModel";
+import { Facebook } from 'react-content-loader/native';
+
 
 const UserPostList: FC<{ navigation: any }> = ({ navigation }) => {
     const [data, setData] = useState<Post[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
   
 
     useEffect(() => {
         const unsubscribe = navigation.addListener('focus', async () => {
-            setIsLoading(true);
-            let posts: Post[] = [];
-            try {
-                posts = await PostModel.getUserPosts();
-            } catch (err) {
-                console.log("Failed fetching posts: " + err);
-                setData([]);
-            }finally {
-                setIsLoading(false);
-              }
-            setData(posts);
+            // setIsLoading(true);
+            // let posts: Post[] = [];
+            // try {
+            //     posts = await PostModel.getUserPosts();
+            // } catch (err) {
+            //     console.log("Failed fetching posts: " + err);
+            //     setData([]);
+            // }finally {
+            //     setTimeout(() => {
+            //         setIsLoading(false);
+            //     }, 1500);
+            //   }
+            // setData(posts);
+            PostPosts();
         });
         return () => unsubscribe();
     }, [navigation]);
+
+    const PostPosts = async () => {
+        setIsLoading(true);
+        let posts: Post[] = [];
+        try {
+            posts = await PostModel.getUserPosts();
+        } catch (err) {
+            console.log("Failed fetching posts: " + err);
+            setData([]);
+        }finally {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 1500);
+          }
+        setData(posts);
+    }
 
     const handleEditPress = (postId: string, postImage: string, postText: string) => {
         // Pass parameters as a single object for consistency
         navigation.navigate('EditPostScreen', { post: { postId, postText, postImage } });
         console.log("Edit post with id: " + postId, postImage, postText);
     };
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        PostPosts();
+        setTimeout(() => {
+          setRefreshing(false);
+        }, 2000);
+      }, []);
     
 
       return (
         <View style={styles.container}>
         {isLoading ? (
-            <ActivityIndicator size="large" color="#007BFF" style={styles.loader} />
-        ) : (
+                <View style={styles.loaderContainer}>
+                    <View style={styles.loader}><Facebook /></View>
+                    <View style={styles.loader}><Facebook /></View>
+                    <View style={styles.loader}><Facebook /></View>
+                </View>
+            ) : (
         <FlatList
+        refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
             style={styles.flatList}
             data={data}
             keyExtractor={(item, index) => item.id || `temp-post-${index}`}
@@ -65,10 +102,12 @@ const styles = StyleSheet.create({
     flatList: {
         flex: 1,
     },
-    loader: {
+    loaderContainer: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+    },
+    loader: {
+        margin: 50,  // Adjust the margin as needed
     },
     icon: {
         marginRight: 8,
